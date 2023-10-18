c559a67040ff356b5e8b1d5d189a04db257a8d41
bengillitt <bengillitt@gmail.com>
2023-10-18 20:21:38 +0100

    Changed Attack

--- contracts/attacker-contracts/AttackPuppet.sol
+++ contracts/attacker-contracts/AttackPuppet.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.0;
 
 contract AttackPuppet {
     address S_puppetPool;
+    address S_token;
 
-    constructor(address _puppetPool) {
+    constructor(address _puppetPool, address _token) {
         S_puppetPool = _puppetPool;
+        S_token = _token;
     }
 }
